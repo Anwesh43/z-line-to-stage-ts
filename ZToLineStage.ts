72d91b5ca43ff2037e8ddf2546e705aae3351950
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 17:56:02 +0530

    adding Animator class

--- ZToLineStage.ts
+++ ZToLineStage.ts
@@ -127,3 +127,23 @@ class State {
         }
     }
 }
+
+class Animator {
+
+    animated : boolean = false
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
