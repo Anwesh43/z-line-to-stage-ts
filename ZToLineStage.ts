534bc8361696b0daa8e5bb2947349c16c008dd45
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 18:03:41 +0530

    adding ZToLine class

--- ZToLineStage.ts
+++ ZToLineStage.ts
@@ -192,3 +192,27 @@ class ZTLNode {
         return this
     }
 }
+
+class ZToLine {
+
+    root : ZTLNode = new ZTLNode(0)
+    curr : ZTLNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
