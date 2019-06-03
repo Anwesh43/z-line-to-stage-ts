53ab5d6175f5ba790df4cd81ba8e8e9e80806e9c
Anwesh43 <anweshthecool0@gmail.com>
2019-06-03 17:22:16 +0530

    adding ZToLineStage class

---
+++ ZToLineStage.ts
@@ -0,0 +1,41 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const nodes : number = 5
+const lines : number = 2
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#1A237E"
+const backColor : string = "#BDBDBD"
+
+class ZToLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = foreColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : ZToLineStage = new ZToLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
